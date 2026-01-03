22ea0197dc5c1b05caec7d6e4ff855da8ead4af7
Filip Vybihal <101115161+filvyb@users.noreply.github.com>
2026-01-03 15:12:27 +0100

    Add tests

--- tests/test1.nim
+++ tests/test1.nim
@@ -1,40 +1,198 @@
-# This is just an example to get you started. You may wish to put all of your
-# tests into a single file, or separate them into multiple `test1`, `test2`
-# etc. files (better names are recommended, just make sure the name starts with
-# the letter 't').
-#
-# To run these tests, simply execute `nimble test`.
-
 import unittest
 import std/asyncdispatch
 import std/options
+import std/os
 
 import Nimbooru
 
-test "sync":
-  var b = initBooruClient(Gelbooru)
-  check b.searchPosts().len == 100
-  b.site = some Safebooru
-  check b.searchPosts().len == 100
-  b.site = some Danbooru
-  check b.searchPosts().len == 100
-  b.site = some Yandere
-  check b.searchPosts().len == 100
-  b.site = some Konachan
-  check b.searchPosts().len == 100
-  b.site = some E621
-  check b.searchPosts().len == 100
+# Helper to validate common BooruImage fields are populated
+proc validateImage(img: BooruImage) =
+  check img.id != ""
+  check img.file_url != "" or img.preview_url != ""  # Some posts may lack file_url
+  check img.height > 0
+  check img.width > 0
+  check img.rating != ""
+  check img.tags.len > 0
 
-test "async":
-  var b = initBooruClient(Gelbooru)
-  check (waitFor b.asyncSearchPosts()).len == 100
-  b.site = some Safebooru
-  check (waitFor b.asyncSearchPosts()).len == 100
-  b.site = some Danbooru
-  check (waitFor b.asyncSearchPosts()).len == 100
-  b.site = some Yandere
-  check (waitFor b.asyncSearchPosts()).len == 100
-  b.site = some Konachan
-  check (waitFor b.asyncSearchPosts()).len == 100
-  b.site = some E621
-  check (waitFor b.asyncSearchPosts()).len == 100
+# Note: Gelbooru requires API key authentication.
+# To test Gelbooru, set GELBOORU_API_KEY and GELBOORU_USER_ID env vars.
+suite "Gelbooru":
+  var apiKey = ""
+  var userId = ""
+
+  # Check for API credentials
+  try:
+    apiKey = getEnv("GELBOORU_API_KEY")
+    userId = getEnv("GELBOORU_USER_ID")
+  except:
+    discard
+
+  test "searchPosts returns posts":
+    if apiKey == "":
+      skip()
+    else:
+      let client = initBooruClient(Gelbooru, apiKey = some apiKey, userId = some userId)
+      let posts = client.searchPosts(limit = 10)
+      check posts.len == 10
+
+  test "searchPosts with pagination":
+    if apiKey == "":
+      skip()
+    else:
+      let client = initBooruClient(Gelbooru, apiKey = some apiKey, userId = some userId)
+      let page0 = client.searchPosts(limit = 5, page = 0)
+      let page1 = client.searchPosts(limit = 5, page = 1)
+      check page0.len == 5
+      check page1.len == 5
+      check page0[0].id != page1[0].id
+
+  test "getPost returns valid image":
+    if apiKey == "":
+      skip()
+    else:
+      let client = initBooruClient(Gelbooru, apiKey = some apiKey, userId = some userId)
+      let posts = client.searchPosts(limit = 1)
+      let post = client.getPost(posts[0].id)
+      validateImage(post)
+      check post.id == posts[0].id
+
+  test "asyncSearchPosts returns posts":
+    if apiKey == "":
+      skip()
+    else:
+      let client = initBooruClient(Gelbooru, apiKey = some apiKey, userId = some userId)
+      let posts = waitFor client.asyncSearchPosts(limit = 10)
+      check posts.len == 10
+
+suite "Safebooru":
+  var client = initBooruClient(Safebooru)
+
+  test "searchPosts returns posts":
+    let posts = client.searchPosts(limit = 10)
+    check posts.len == 10
+
+  test "searchPosts with pagination":
+    let page0 = client.searchPosts(limit = 5, page = 0)
+    let page1 = client.searchPosts(limit = 5, page = 1)
+    check page0.len == 5
+    check page1.len == 5
+    check page0[0].id != page1[0].id
+
+  test "getPost returns valid image":
+    let posts = client.searchPosts(limit = 1)
+    let post = client.getPost(posts[0].id)
+    validateImage(post)
+
+  test "asyncSearchPosts returns posts":
+    let posts = waitFor client.asyncSearchPosts(limit = 10)
+    check posts.len == 10
+
+suite "Danbooru":
+  var client = initBooruClient(Danbooru)
+
+  test "searchPosts returns posts":
+    let posts = client.searchPosts(limit = 10)
+    check posts.len == 10
+
+  test "searchPosts with pagination":
+    let page0 = client.searchPosts(limit = 5, page = 1)
+    let page1 = client.searchPosts(limit = 5, page = 2)
+    check page0.len == 5
+    check page1.len == 5
+    check page0[0].id != page1[0].id
+
+  test "getPost returns valid image":
+    let posts = client.searchPosts(limit = 1)
+    let post = client.getPost(posts[0].id)
+    check post.id == posts[0].id
+    check post.rating != ""
+    check post.tags.len > 0
+
+  test "asyncSearchPosts returns posts":
+    let posts = waitFor client.asyncSearchPosts(limit = 10)
+    check posts.len == 10
+
+suite "Yandere":
+  var client = initBooruClient(Yandere)
+
+  test "searchPosts returns posts":
+    let posts = client.searchPosts(limit = 10)
+    check posts.len == 10
+
+  test "searchPosts with pagination":
+    let page0 = client.searchPosts(limit = 5, page = 1)
+    let page1 = client.searchPosts(limit = 5, page = 2)
+    check page0.len == 5
+    check page1.len == 5
+    check page0[0].id != page1[0].id
+
+  test "getPost returns valid image":
+    let posts = client.searchPosts(limit = 1)
+    let post = client.getPost(posts[0].id)
+    validateImage(post)
+
+  test "asyncSearchPosts returns posts":
+    let posts = waitFor client.asyncSearchPosts(limit = 10)
+    check posts.len == 10
+
+suite "Konachan":
+  var client = initBooruClient(Konachan)
+
+  test "searchPosts returns posts":
+    let posts = client.searchPosts(limit = 10)
+    check posts.len == 10
+
+  test "searchPosts with pagination":
+    let page0 = client.searchPosts(limit = 5, page = 1)
+    let page1 = client.searchPosts(limit = 5, page = 2)
+    check page0.len == 5
+    check page1.len == 5
+    check page0[0].id != page1[0].id
+
+  test "getPost returns valid image":
+    let posts = client.searchPosts(limit = 1)
+    let post = client.getPost(posts[0].id)
+    validateImage(post)
+
+  test "asyncSearchPosts returns posts":
+    let posts = waitFor client.asyncSearchPosts(limit = 10)
+    check posts.len == 10
+
+suite "E621":
+  var client = initBooruClient(E621)
+
+  test "searchPosts returns posts":
+    let posts = client.searchPosts(limit = 10)
+    check posts.len == 10
+
+  test "searchPosts with pagination":
+    let page0 = client.searchPosts(limit = 5, page = 1)
+    let page1 = client.searchPosts(limit = 5, page = 2)
+    check page0.len == 5
+    check page1.len == 5
+    check page0[0].id != page1[0].id
+
+  test "searchPosts with tags":
+    let posts = client.searchPosts(limit = 5, tags = some @["canine"])
+    check posts.len > 0
+
+  test "getPost returns valid image":
+    let posts = client.searchPosts(limit = 1)
+    let post = client.getPost(posts[0].id)
+    check post.id == posts[0].id
+    check post.tags.len > 0
+
+  test "asyncSearchPosts returns posts":
+    let posts = waitFor client.asyncSearchPosts(limit = 10)
+    check posts.len == 10
+
+suite "Error handling":
+  test "missing site raises error":
+    var client: BooruClient
+    expect BooruError:
+      discard client.searchPosts()
+
+  test "invalid post ID raises BooruNotFoundError":
+    let client = initBooruClient(Safebooru)
+    expect BooruNotFoundError:
+      discard client.getPost("999999999999")
